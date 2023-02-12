5c0f96d2227becd3b81d21efdcba1c727d7ec7e3
leksmi <alexdotspb@gmail.com>
2023-02-12 12:00:50 +0300

    pyneng_exercises: task_6_1 done

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -13,3 +13,5 @@ MAC-адреса используются в формате XXXX.XXXX.XXXX
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+result = [macaddr.replace(':', '.') for macaddr in mac ]
+print(result)
